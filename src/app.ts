062c7233f4d187e4ab9c14f881c21cbcca8e9e50
Develeon64 <software@develeon.de>
2021-12-17 17:59:08 +0100

    Verzeichnis-Struktur gewährleisten

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,17 @@
 import { RoboJuno } from "./bot.ts";
+
+Deno.mkdirSync("logs", { recursive: true });
+Deno.mkdirSync("var", { recursive: true });
+Deno.mkdirSync("var/conf", { recursive: true });
+Deno.mkdirSync("var/db", { recursive: true });
+try {
+	Deno.statSync("var/conf/config.json");
+}
+catch (error) {
+	if (error && error.kind === Deno.errors.NotFound)
+		Deno.writeTextFileSync("var/conf/config.json", "{}", { create: true });
+	else
+		throw error;
+}
+
 const _bot: RoboJuno = new RoboJuno();
