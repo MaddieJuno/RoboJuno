6b9b500f097f19c612f3d03e7691714496bcc91c
Develeon64 <software@develeon.de>
2022-01-02 21:00:55 +0100

    Neue Versionsnummern

--- src/discord.ts
+++ src/discord.ts
@@ -45,7 +45,7 @@ interface BotOptions extends ClientOptions {
 
 export class DiscordBot extends Client {
 	private syncCommands: boolean;
-	private readonly VERSION = "v1.0.0.8";
+	private readonly VERSION = "v1.1.0.9";
 	private preCounter = 0;
 	private isPre = false;
 
